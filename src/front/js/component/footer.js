ab68f01e219c9a74ac0eb94b3bee985ac4c7ff8f
Vero2112 <verorodriguezruiz21@gmail.com>
2022-07-09 17:17:57 +0000

    footer acabado

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-		</p>
-	</footer>
+  <footer className="footer mt-auto py-3 text-center">
+    <p>
+      Made with <i className="fa fa-heart text-danger" /> by{" "}
+      <a href="https://github.com/isarebollo">Isabel Rebollo, </a>
+      <a href="https://github.com/aleona-e">Alejandra Elsin, </a>
+      <a href="https://github.com/Vero2112">Verónica Rodríguez</a>
+    </p>
+  </footer>
 );
