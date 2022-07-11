bd1bd8df06ebbbf97b1458f04688ad80b71d7f13
isarebollo <isarebollo@gmail.com>
2022-07-11 18:56:48 +0000

    Login redirige a pagina + css

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
 
 import { Home } from "./pages/home";
-import { Register  } from "./pages/register.jsx";
+import { Register } from "./pages/register.jsx";
 import { Single } from "./pages/single";
 import { Login } from "./pages/login.jsx";
 import { HomeCardGroup } from "./component/homecardgroup.jsx";
@@ -18,36 +18,35 @@ import injectContext from "./store/appContext";
 import { Navbar } from "./component/navbar.jsx";
 import { Footer } from "./component/footer.jsx";
 
-
 //create your first component
 const Layout = () => {
-    //the basename is used when your project is published in a subdirectory and not in the root of the domain
-    // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
-    const basename = process.env.BASENAME || "";
+  //the basename is used when your project is published in a subdirectory and not in the root of the domain
+  // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
+  const basename = process.env.BASENAME || "";
 
-    return (
-        <div>
-            <BrowserRouter basename={basename}>
-                <ScrollToTop>
-                    <Navbar />
-                    <Routes>
-                        <Route element={<Home />} path="/" />
-                        <Route element={<Register />} path="/register" />
-                        <Route element={<Login />} path="/login" />
-                        <Route element={<HomeCardGroup />} path="/homecardgroup" />
-                        <Route element={<Actividades />} path="/actividades" />
-                        <Route element={<EventosCreados />} path="/eventoscreados" />
-                        <Route element={<Learnmore />} path="/learnmore" />
-                        <Route element={<MisEventos />} path="/miseventos" />
-                        <Route element={<MiPerfil />} path="/miperfil" />
-                        <Route element={<Single />} path="/single/:theid" />
-                        <Route element={<h1>Not found!</h1>} />
-                    </Routes>
-                    <Footer />
-                </ScrollToTop>
-            </BrowserRouter>
-        </div>
-    );
+  return (
+    <div>
+      <BrowserRouter basename={basename}>
+        <ScrollToTop>
+          <Navbar />
+          <Routes>
+            <Route element={<Home />} path="/" />
+            <Route element={<Register />} path="/register" />
+            <Route element={<Login />} path="/login" />
+            <Route element={<HomeCardGroup />} path="/homecardgroup" />
+            <Route element={<Actividades />} path="/actividades" />
+            <Route element={<EventosCreados />} path="/eventoscreados" />
+            <Route element={<Learnmore />} path="/learnmore" />
+            <Route element={<MisEventos />} path="/miseventos" />
+            <Route element={<MiPerfil />} path="/miperfil" />
+            <Route element={<Single />} path="/single/:theid" />
+            <Route element={<h1>Not found!</h1>} />
+          </Routes>
+          <Footer />
+        </ScrollToTop>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default injectContext(Layout);
