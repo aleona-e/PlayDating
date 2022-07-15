32ac787e1309a6a3c8555c407950e6fff7348f40
aleona-e <ale.elsin92@gmail.com>
2022-07-15 18:06:02 +0000

    nombres actividades arreglados y link en navbar a actividades

--- src/front/js/component/navbar.jsx
+++ src/front/js/component/navbar.jsx
@@ -1,15 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logoUrl from "../../img/logo-circulo.png";
+import logoDosUrl from "../../img/logo-castillo.png";
 
 export const Navbar = () => {
   return (
     <nav className="navbar navbar-ligth bg-light">
       <div className="container">
         <Link to="/">
-          <span className="navbar-brand mb-0 h1">PlayDating</span>
+          <span className="navbar-brand mb-0 h1"> 
+          <img
+            src={logoDosUrl}
+            alt=""
+            width="120"
+            height="100"
+            className="d-inline-block align-text-top">
+          </img></span>
         </Link>
         <div>
+        <Link to="/actividades"> 
           <h5>Actividades</h5>
+          </Link>
         </div>
         <div>
           <h5>Eventos</h5>
